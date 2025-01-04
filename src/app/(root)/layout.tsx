16e4c45690e4fa9be6027c780d777474c0ae6a2b
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-04 18:18:52 +0100

    feat: add and use HEader component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header/Header';
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,6 +6,7 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-screen flex-col">
+      <Header />
       <main className="wrapper flex-1">{children}</main>
     </div>
   );
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,42 @@
+import { ShoppingCart, UserIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { APP_TITLE } from '@/lib/constants';
+
+import { Button } from '../ui/button';
+
+export default function Header() {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+            <Image
+              src="images/logo.svg"
+              alt={`${APP_TITLE} logo`}
+              height={48}
+              width={48}
+              priority={true}
+            />
+            <span className="ml-3 hidden text-2xl font-bold lg:block">
+              {APP_TITLE}
+            </span>
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant="ghost">
+            <Link href="/cart">
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild variant="ghost">
+            <Link href="/sign-in">
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
