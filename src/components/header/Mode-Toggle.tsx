6747e3d51ae02b8c6528b72013d71b67226cd88c
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-08 18:42:42 +0100

    feat: create mode-toggle component

---
+++ src/components/header/Mode-Toggle.tsx
@@ -0,0 +1,73 @@
+'use client';
+import { MoonIcon, SunIcon, SunMoon } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import React, { JSX } from 'react';
+
+import { Button } from '../ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+
+type ThemeOption = 'system' | 'light' | 'dark';
+type ThemeSetter = JSX.Element;
+
+function setThemeIcon(theme: ThemeOption): ThemeSetter {
+  switch (theme) {
+    case 'system': {
+      return <SunMoon />;
+    }
+    case 'dark': {
+      return <MoonIcon />;
+    }
+    case 'light': {
+      return <SunIcon />;
+    }
+    default: {
+      return <SunMoon />;
+    }
+  }
+}
+
+export default function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="focus-visible:ring-0 focus-visible:ring-offset-0"
+        >
+          {setThemeIcon(theme as ThemeOption)}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={theme === 'system'}
+          onClick={() => setTheme('system')}
+        >
+          System
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={theme === 'light'}
+          onClick={() => setTheme('light')}
+        >
+          Light
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={theme === 'dark'}
+          onClick={() => setTheme('dark')}
+        >
+          Dark
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
