286b489d113d222e6270b0c564dac4444670cd0a
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-09 16:31:42 +0100

    style: add gap-4 for ProductCard wrapper

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -5,7 +5,7 @@ export default function ProductsList({ data, title }) {
   return (
     <div className="my-10">
       <h2 className="h2-bold mb-4">{title}</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data.map(product => (
           <ProductCard key={product.slug} product={product} />
         ))}
