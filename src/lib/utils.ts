829831e7c22f7686316690acc1bba75affb0f523
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-10 11:51:28 +0100

    feat: add convertToPlainObject function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function convertToPlainObject<T>(object: T): T {
+  /* eslint-disable-next-line unicorn/prefer-structured-clone */
+  return JSON.parse(JSON.stringify(object));
+}
