d4636f87ec7c02db4578d85ad83021cc5ba17437
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-14 20:01:21 +0100

    chore: add sample users and seed db

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -5,7 +5,14 @@ import sampleData from './sample-data';
 const prisma = new PrismaClient();
 
 await prisma.product.deleteMany();
+await prisma.account.deleteMany();
+await prisma.session.deleteMany();
+await prisma.verificationToken.deleteMany();
+await prisma.user.deleteMany();
 
 await prisma.product.createMany({
   data: sampleData.products,
+});
+await prisma.user.createMany({
+  data: sampleData.users,
 });
