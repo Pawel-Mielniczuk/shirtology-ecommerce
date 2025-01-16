88ed0557f42c5ac1e011d3a658ecd1b191ecf788
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-16 19:26:37 +0100

    feat: use user-button in menu component

--- src/components/header/Menu.tsx
+++ src/components/header/Menu.tsx
@@ -1,4 +1,4 @@
-import { EllipsisVertical, ShoppingCart, UserIcon } from 'lucide-react';
+import { EllipsisVertical, ShoppingCart } from 'lucide-react';
 import Link from 'next/link';
 
 import { Button } from '../ui/button';
@@ -10,6 +10,7 @@ import {
   SheetTrigger,
 } from '../ui/sheet';
 import ModeToggle from './Mode-Toggle';
+import { UserButton } from './user-button';
 
 export default function Menu() {
   return (
@@ -21,11 +22,7 @@ export default function Menu() {
             <ShoppingCart /> Cart
           </Link>
         </Button>
-        <Button asChild>
-          <Link href="/sign-in">
-            <UserIcon /> Sign In
-          </Link>
-        </Button>
+        <UserButton />
       </nav>
       <nav className="md:hidden">
         <Sheet>
@@ -40,11 +37,7 @@ export default function Menu() {
                 <ShoppingCart /> Cart
               </Link>
             </Button>
-            <Button asChild>
-              <Link href="/sign-in">
-                <UserIcon /> Sign In
-              </Link>
-            </Button>
+            <UserButton />
             <SheetDescription></SheetDescription>
           </SheetContent>
         </Sheet>
