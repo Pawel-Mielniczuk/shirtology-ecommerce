c8ddc1fec1e5d14387a4c6628840d554e2b9a970
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-16 17:45:20 +0100

    feat(validator.ts): add signIn schema

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -11,3 +11,8 @@ export const insertProductSchema = z.object({
   banner: z.string().nullable(),
   price: z.number(),
 });
+
+export const signInSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+});
