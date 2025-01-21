7747740807bfbf4245a3bb55a8dc65c459a3af20
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-21 12:18:08 +0100

    feat(auth.ts): customize token with jwt callback

--- auth.ts
+++ auth.ts
@@ -57,12 +57,32 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       //set userId from token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
       // If there is an update, set the user name
       if (trigger === 'update') {
         session.user.name = user.name;
       }
       return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      //Assign user fields to token
+      if (user) {
+        token.role = user.role;
+
+        //If user has no name then use the email
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0];
+
+          // Update db to reflect the token
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: user.name },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
