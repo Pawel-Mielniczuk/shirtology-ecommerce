5488d1b666b6c18570226990f45c08fa9a4acf1e
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-21 11:26:29 +0100

    feat(validator.ts): add schema for sing-up action

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -16,3 +16,15 @@ export const signInSchema = z.object({
   email: z.string().email('Invalid email address'),
   password: z.string().min(6, 'Password must be at least 6 characters'),
 });
+
+export const signUpSchema = z
+  .object({
+    name: z.string().min(3, 'Name must be at least 3'),
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+    confirmPassword: z.string().min(6, 'COnfirm password'),
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    message: 'Password do not match',
+    path: ['confirmPassword'],
+  });
