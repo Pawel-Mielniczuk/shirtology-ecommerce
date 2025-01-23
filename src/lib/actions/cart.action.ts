8346ccd78aecb894de3e32de1ebafd8228ba3861
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-23 13:29:12 +0100

    style: remove commented code

--- src/lib/actions/cart.action.ts
+++ src/lib/actions/cart.action.ts
@@ -154,32 +154,3 @@ export async function getMyCart() {
     taxPrice: cart.taxPrice.toString(),
   });
 }
-
-// export async function getMyCart() {
-//   // Check for cart cookie
-//   const sessionCartId = (await cookies()).get('sessionCartId')?.value;
-//   if (!sessionCartId) throw new Error('Cart session not found');
-
-//   // Get session and user ID
-//   const session = await auth();
-//   const userId = session?.user?.id ? (session.user.id as string) : undefined;
-
-//   // Get user cart from database
-//   const cart = await prisma.cart.findFirst({
-//     where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
-//   });
-
-//   console.log('cart is', cart);
-
-//   if (!cart) return undefined;
-
-//   // Convert decimals and return
-//   return convertToPlainObject({
-//     ...cart,
-//     items: cart.items as CartItem[],
-//     itemsPrice: cart.itemsPrice.toString(),
-//     totalPrice: cart.totalPrice.toString(),
-//     shippingPrice: cart.shippingPrice.toString(),
-//     taxPrice: cart.taxPrice.toString(),
-//   });
-// }
