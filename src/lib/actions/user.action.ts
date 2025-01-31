cfb7e240aee18a4d3165a042ec9f84ae7f127ae5
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-31 19:45:39 +0100

    feat: create user action: updateUserAddress

--- src/lib/actions/user.action.ts
+++ src/lib/actions/user.action.ts
@@ -4,10 +4,15 @@ import { hashSync } from 'bcrypt-ts-edge';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
 
 import { prisma } from '@/db/prisma';
+import { ShippingAddress } from '@/types';
 
-import { signIn, signOut } from '../../../auth';
+import { auth, signIn, signOut } from '../../../auth';
 import { formatError } from '../utils';
-import { signInSchema, signUpSchema } from '../validators';
+import {
+  shippingAddressSchema,
+  signInSchema,
+  signUpSchema,
+} from '../validators';
 
 type SignInResult = {
   success: boolean;
@@ -84,4 +89,24 @@ export async function getUserById(userId: string) {
   }
 
   return user;
+}
+
+export async function updateUserAddress(data: ShippingAddress) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+    if (!currentUser) throw new Error('user not found');
+
+    const address = shippingAddressSchema.parse(data);
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: address,
+    });
+
+    return { success: true, message: 'User updates successfully' };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
 }
