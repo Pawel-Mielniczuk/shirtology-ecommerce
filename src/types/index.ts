d575f3d3ba45f63daa37a4020801e9774485285f
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-31 19:44:38 +0100

    refactor: fix typo in shippingAddress type

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,7 +12,7 @@ export type Product = z.infer<typeof insertProductSchema> & {
   rating: string;
   createdAt: Date;
 };
-export type shippingAddress = z.infer<typeof shippingAddressSchema>;
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
 
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
