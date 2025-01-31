71546937dd05fb1c8132c46abd49e6c7495792bd
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-31 14:35:53 +0100

    feat: create user action: getUserById

--- src/lib/actions/user.action.ts
+++ src/lib/actions/user.action.ts
@@ -73,3 +73,15 @@ export async function signUpUser(previousState: unknown, formData: FormData) {
     return { success: false, message: formatError(error) };
   }
 }
+
+export async function getUserById(userId: string) {
+  const user = await prisma.user.findFirst({
+    where: { id: userId },
+  });
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  return user;
+}
