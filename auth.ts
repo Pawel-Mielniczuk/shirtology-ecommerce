433272ce406a50ca13d620eed1dc8988cab8fab7
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-01-31 20:09:52 +0100

    refactor: fix persist session cart

--- auth.ts
+++ auth.ts
@@ -83,6 +83,27 @@ export const config = {
             data: { name: user.name },
           });
         }
+
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
+        }
       }
       return token;
     },
